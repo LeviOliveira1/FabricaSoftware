4b623d812e9523aad9a24a2b417f95e64e9269f1
Levi Oliveira <110423768+LeviOliveira1@users.noreply.github.com>
2022-09-16 18:06:49 -0300

    finalizando projectfab

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ChuckNorrisPage from "./routes/chuckNorris.page";
+import DadJokePage from "./routes/dadJoke.page";
+import Navbar from "./components/navbar";
+import "./index.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    children: [
+      {
+        path: "/",
+        element: <ChuckNorrisPage />,
+      },
+      {
+        path: "/dadJoke",
+        element: <DadJokePage />,
+      },
+    ],
+  },
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
